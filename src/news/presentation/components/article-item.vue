6131d5bcbfdd81c2e6174f2d78626aa1586bcea6
Angel Velasquez <pcisavel@upc.edu.pe>
2025-09-14 20:39:46 -0500

    feat(news): add article-item component for displaying article details and sharing functionality.

---
+++ src/news/presentation/components/article-item.vue
@@ -0,0 +1,90 @@
+<script lang="js" setup>
+import {useI18n} from "vue-i18n";
+import {Article} from "../../domain/model/article.entity.js";
+
+const {t} = useI18n();
+
+const props = defineProps({article: {type: Article, required: true}});
+const {article} = props;
+const emit = defineEmits(['tooltip-showed']);
+
+async function shareArticle() {
+  const shareData = {title: this.article.title, url: this.article.url};
+  if (navigator.share) {
+    try {
+      await navigator.share(shareData);
+      console.log('Article shared successfully');
+    } catch (err) {
+      console.error('Error sharing the article:', err);
+    }
+  } else {
+    try {
+      await navigator.clipboard.writeText(this.article.url);
+      emit('tooltip-showed', this.article.url);
+      console.log('Article URL copied to clipboard');
+    } catch (err) {
+      console.error('Failed to copy the article URL:', err);
+    }
+  }
+}
+
+</script>
+
+<template>
+  <pv-card class="m-2">
+    <template #header>
+      <img :alt="article.title" :src="article.urlToImage" class="image-fit"/>
+    </template>
+    <template #title>
+      <p class="flex align-content-start flex-wrap">
+        {{ article.title }}
+      </p>
+    </template>
+    <template #subtitle>
+      <p class="flex align-content-start flex-wrap">
+        <span class="flex align-items-center justify-content-center mr-2">
+          <pv-avatar :aria-label="article.source.name"
+                     :image="article.source.urlToLogo"
+                     shape="circle"/>
+        </span>
+        <span class="flex align-items-center justify-content-center">
+          {{ article.source.name }}
+        </span>
+
+      </p>
+      <p class="flex align-content-start flex-wrap">
+        <span class="text-sm">{{ article.getFormatedPublishedAt() }}</span>
+      </p>
+    </template>
+    <template #footer>
+      <div class="flex justify-content -webkit-box-sizing: border-box; justify-content: space-between;">
+        <a :href="article.url" target="_blank">{{ t('read-more') }}</a>
+        <span class="p-spacer"></span>
+        <pv-button
+            v-tooltip="t('article.copy-to-clipboard')"
+            :label="t('article.share')"
+            aria-label="Share article"
+            class="p-button-text p-button-sm"
+            icon="pi pi-share-alt"
+            @click="shareArticle"/>
+      </div>
+    </template>
+  </pv-card>
+</template>
+
+<style scoped>
+.p-button-sm {
+  font-size: 0.875rem;
+  padding: 0.5rem 1rem;
+}
+
+.p-spacer {
+  flex: 1 1 auto;
+}
+
+.image-fit {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+</style>
