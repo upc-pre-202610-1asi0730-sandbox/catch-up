c73d460162423d451b93802e56f8d175b6a8cf3c
Angel Velasquez <pcisavel@upc.edu.pe>
2025-09-14 20:39:04 -0500

    feat(news): create news api class for fetching news sources and articles.

---
+++ src/news/infrastructure/news-api.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const newsApi               = import.meta.env.VITE_NEWS_API_URL;
+const apiKey                = import.meta.env.VITE_NEWS_API_KEY;
+const sourcesEndpoint       = import.meta.env.VITE_SOURCES_ENDPOINT_PATH;
+const topHeadlinesEndpoint  = import.meta.env.VITE_TOP_HEADLINES_ENDPOINT_PATH;
+
+const http = axios.create({
+    baseURL: newsApi,
+    params: {
+        apiKey: apiKey,
+    },
+})
+
+export class NewsApi {
+
+    getSources() {
+        return http.get(`${sourcesEndpoint}`);
+    }
+
+    getArticlesForSourceId(sourceId) {
+        return http.get(`${topHeadlinesEndpoint}`, {params: {sources: sourceId}});
+    }
+
+}
