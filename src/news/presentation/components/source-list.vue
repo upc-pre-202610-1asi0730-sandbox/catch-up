28130ebfbbf1e441ce1050c413b8eced3bec1236
Angel Velasquez <pcisavel@upc.edu.pe>
2025-09-14 20:39:35 -0500

    feat(news): add source-list component for displaying a list of sources.

---
+++ src/news/presentation/components/source-list.vue
@@ -0,0 +1,25 @@
+<script setup lang="js">
+  import {Source} from "../../domain/model/source.entity.js";
+  import SourceItem from "./source-item.vue";
+
+  const props = defineProps({ visible: Boolean, sources: Array[Source] });
+  const emit  = defineEmits(['source-selected']);
+
+  function emitSourceSelectedEvent(source) {
+    emit('source-selected', source);
+  }
+
+</script>
+
+<template>
+  <pv-drawer v-bind:visible="visible">
+    <source-item v-for="source in sources"
+                 :key="source.id"
+                 :source="source"
+                 @source-selected="emitSourceSelectedEvent(source)"/>
+  </pv-drawer>
+</template>
+
+<style scoped>
+
+</style>
