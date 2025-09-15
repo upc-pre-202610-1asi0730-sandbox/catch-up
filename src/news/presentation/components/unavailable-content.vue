768bc8f4dd2cf4535057d331d97c5ffb173aafc1
Angel Velasquez <pcisavel@upc.edu.pe>
2025-09-14 20:40:02 -0500

    feat(news): add unavailable-content component for displaying error messages when news is unavailable.

---
+++ src/news/presentation/components/unavailable-content.vue
@@ -0,0 +1,19 @@
+<script setup lang="js">
+  import {useI18n} from "vue-i18n";
+  const { t } = useI18n();
+  const props = defineProps({ errors: null});
+  const { errors } = props;
+</script>
+
+<template>
+  <div>
+    <div><h4>{{ t('unavailable-news') }}</h4></div>
+    <div v-for="error in errors">
+      <h6>{{ error }}</h6>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
