95abe091c1cbc26acbe1b342b80078438cd74670
Angel Velasquez <pcisavel@upc.edu.pe>
2025-09-14 20:39:25 -0500

    feat(news): add source-item component for displaying source details.

---
+++ src/news/presentation/components/source-item.vue
@@ -0,0 +1,29 @@
+<script setup lang="js">
+  import {Source} from "../../domain/model/source.entity.js";
+
+  const props = defineProps({ source: { type: Source, required: true } });
+  const emit  = defineEmits(['source-selected']);
+
+  function emitSourceSelectedEvent() {
+    emit('source-selected', props.source);
+  }
+</script>
+
+<template>
+  <div class="m-4">
+    <div @click="emitSourceSelectedEvent" class="flex align-content-start flex-wrap">
+    <span  class="flex align-items-center justify-content-center mr-2">
+          <pv-avatar :aria-label="source.name"
+                     :image="source.urlToLogo"
+                     shape="circle"/>
+    </span>
+      <span  class="flex align-items-center justify-content-center">
+          {{source.name}}
+    </span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
