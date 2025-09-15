e595456ff602d4b2cdc2a6dd7b141ef139952140
Angel Velasquez <pcisavel@upc.edu.pe>
2025-09-14 20:40:34 -0500

    feat(ui): add additional prime vue components for user interface functionality.

--- src/main.js
+++ src/main.js
@@ -6,14 +6,20 @@ import PrimeVue from 'primevue/config';
 import Material from '@primeuix/themes/material';
 import 'primeicons/primeicons.css';
 import 'primeflex/primeflex.css';
-import {Button, SelectButton} from "primevue";
+import {Avatar, Button, Card, Drawer, Image, Menu, Menubar, SelectButton, Toolbar, Tooltip} from "primevue";
+
 
 createApp(App)
     .use(i18n)
-    .use(PrimeVue, {
-        ripple: true,
-        theme: { preset: Material}
-    })
+    .use(PrimeVue, { ripple: true, theme: { preset: Material }})
     .component('pv-button', Button)
     .component('pv-select-button', SelectButton)
+    .component('pv-avatar', Avatar)
+    .component('pv-drawer', Drawer)
+    .component('pv-card', Card)
+    .component('pv-image', Image)
+    .component('pv-toolbar', Toolbar)
+    .component('pv-menu', Menu)
+    .component('pv-menubar', Menubar)
+    .directive('tooltip', Tooltip)
     .mount('#app')
