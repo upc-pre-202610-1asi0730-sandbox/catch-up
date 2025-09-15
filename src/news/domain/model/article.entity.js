8cec35b462156264a37bb11afe7dd6cc31ec95b6
Angel Velasquez <pcisavel@upc.edu.pe>
2025-09-14 20:37:34 -0500

    feat(news): create Article entity with formatting method.

---
+++ src/news/domain/model/article.entity.js
@@ -0,0 +1,22 @@
+import {Source} from "./source.entity.js";
+
+export class Article {
+    constructor({title = '', description = '', url = '', urlToImage = '', source = null, publishedAt = ''}) {
+        this.title = title;
+        this.description = description;
+        this.url = url;
+        this.urlToImage = urlToImage;
+        this.source = source ? new Source(source) : null;
+        this.publishedAt = new Date(publishedAt);
+    }
+
+    getFormatedPublishedAt() {
+        return this.publishedAt.toLocaleDateString('en-US', {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit'
+        });
+    }
+}
