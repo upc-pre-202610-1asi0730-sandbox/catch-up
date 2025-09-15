7d435e03e1cee819393e24a6b70e5b8c9925a2d8
Angel Velasquez <pcisavel@upc.edu.pe>
2025-09-14 20:39:54 -0500

    feat(news): add article-list component for rendering a list of articles.

---
+++ src/news/presentation/components/article-list.vue
@@ -0,0 +1,17 @@
+<script setup lang="js">
+import ArticleItem from "./article-item.vue";
+
+const props = defineProps({ articles: { type: Array, required: true } });
+const { articles } = props;
+
+</script>
+
+<template>
+  <div v-for="article in articles">
+    <article-item :article="article"/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
